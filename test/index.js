bc16d327d47397688c0282f55ce730defe692913
nulltask <nulltask@gmail.com>
2012-03-18 00:16:08 +0900

    add test for separator

--- test/index.js
+++ test/index.js
@@ -33,4 +33,16 @@ describe('express-csv', function() {
         done();
       });
   });
+
+  it('should return tsv', function(done) {
+    var prevSeparator = csv.separator;
+    csv.separator = '\t';
+    request
+      .get('http://127.0.0.1:8383/test/1')
+      .end(function(res) {
+        csv.separator = prevSeparator;
+        res.text.should.equal('"a"\t"b"\t"c"\r\n"d"\t"e"\t"f"\r\n');
+        done();
+      });
+  });
 });
